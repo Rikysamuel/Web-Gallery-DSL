f39db5787514e98409da1a684aa27393a8ef9b5f
Rikysamuel <rikz.samuel@gmail.com>
2015-11-27 19:36:04 +0700

    Web Gallery DSL

--- src/main/groovy/HTMLGenerator.groovy
+++ src/main/groovy/HTMLGenerator.groovy
@@ -4,113 +4,356 @@ import groovy.xml.MarkupBuilder
  * Created by rikysamuel on 11/27/2015.
  */
 class HTMLGenerator {
-    String toText
-    String fromText
-    String body
-    def sections = []
+    String name
+    String description
+    String profilePicPath
+    boolean login
+    boolean upload
+    int template
 
-    /**
-     * This method accepts a closure which is essentially the DSL. Delegate the closure methods to
-     * the DSL class so the calls can be processed
-     */
+    def static writer = new StringWriter()
+
     def static make(closure) {
         HTMLGenerator memoDsl = new HTMLGenerator()
-        // any method called in closure will be delegated to the memoDsl class
         closure.delegate = memoDsl
         closure()
     }
 
-    /**
-     * Store the parameter as a variable and use it later to output a memo
-     */
-    def to(String toText) {
-        this.toText = toText
+    def name(String name) {
+        this.name = name
     }
 
-    def from(String fromText) {
-        this.fromText = fromText
+    def picture(String picPath) {
+        this.profilePicPath = picPath
     }
 
-    def body(String bodyText) {
-        this.body = bodyText
+    def description(String description) {
+        this.description = description
     }
 
-    /**
-     * When a method is not recognized, assume it is a title for a new section. Create a simple
-     * object that contains the method name and the parameter which is the body.
-     */
-    def methodMissing(String methodName, args) {
-        def section = new Section(title: methodName, body: args[0])
-        sections << section
+    def login(boolean useLogin) {
+        this.login = useLogin
     }
 
-    /**
-     * 'get' methods get called from the dsl by convention. Due to groovy closure delegation,
-     * we had to place MarkUpBuilder and StringWrite code in a static method as the delegate of the closure
-     * did not have access to the system.out
-     */
-    def getXml() {
-        doXml(this)
+    def upload(boolean useUpload) {
+        this.upload = useUpload
+    }
+
+    def template(int template) {
+        this.template = template
     }
 
     def getHtml() {
-        doHtml(this)
+        if (name != null) {
+            if (description != null) {
+                if (profilePicPath != null) {
+                    if (template > 0) {
+                        doHtml(this)
+                    } else {
+                        println "template is not defined!"
+                    }
+                } else {
+                    println "profile picture path is not defined!"
+                }
+            } else {
+                println "description is not defined!"
+            }
+        } else {
+            println "name is not defined!"
+        }
     }
 
-    def getText() {
-        doText(this)
-    }
+    def static templateOne(HTMLGenerator htmlDsl) {
+        def markup = new MarkupBuilder(writer)
 
-    /**
-     * Use markupBuilder to create an html xml output
-     */
-    private static doHtml(HTMLGenerator memoDsl) {
-        def writer = new StringWriter()
-        def xml = new MarkupBuilder(writer)
-        xml.html() {
+        markup.html() {
             head {
-                title("Memo")
+                title("Web Gallery")
+                link(href: "assets/css/bootstrap.min.css", rel: "stylesheet")
+                link(href: "assets/css/bootstrap-theme.min.css", rel:"stylesheet")
             }
             body {
-                h1("Memo")
-                h3("To: ${memoDsl.toText}")
-                h3("From: ${memoDsl.fromText}")
-                p(memoDsl.body)
-                // cycle through the stored section objects and create uppercase/bold section with body
-                for (s in memoDsl.sections) {
-                    p {
-                        b(s.title.toUpperCase())
+                div class: "container", {
+                    div class: "row", {
+                        div class: "col-md-5", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-3", {
+                            h1("Web Gallery")
+                        }
+                        div class: "col-md-4", {
+                            if (htmlDsl.login) {
+                                div class: "row", {
+                                    form class: "form-horizontal", role: "form", id: "proceed", action: "javascript:;", {
+                                        div class: "form-group", {
+                                            label for: "username", class: "col-sm-4 control-label", {
+                                                mkp.yield "Username"
+                                            }
+                                            div class: "col-sm-8", {
+                                                input type: "input", class: "form-control", id:"username", placeholder:"Username"
+                                            }
+                                        }
+                                        div class: "form-group", {
+                                            label for: "password", class: "col-sm-4 control-label", {
+                                                mkp.yield "Password"
+                                            }
+                                            div class: "col-sm-8", {
+                                                input type: "password", class: "form-control", id:"password", placeholder:"Password"
+                                            }
+                                        }
+                                        div class: "form-group", {
+                                            div class: "col-sm-offset-5 col-sm-7", {
+                                                input type: "submit", class: "btn btn-default", value: "Login"
+                                            }
+                                        }
+                                    }
+                                }
+                            } else {
+                                mkp.yield ""
+                            }
+                        }
                     }
-                    p(s.body)
+
+                    div class: "row", {
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-4", {
+                            a href: "#", class: "thumbnail", {
+                                img(src: htmlDsl.profilePicPath, alt:"picture")
+                            }
+                        }
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                    }
+
+                    div class: "row", {
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-4", {
+                            div class: "row", {
+                                div class: "col-md-2", {
+                                    mkp.yield ""
+                                }
+                                div class: "col-md-9", {
+                                    h3(htmlDsl.name)
+                                }
+                                div class: "col-md-1", {
+                                    mkp.yield ""
+                                }
+                            }
+                        }
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                    }
+
+                    div class: "row", {
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-4", {
+                            p(style:"margin-top:2em; font-style: italic; font-size: 12px;", htmlDsl.description)
+                        }
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                    }
                 }
             }
         }
-//        println writer
-
-        new File("tes.html") << writer;
-
     }
 
-    /**
-     * Use markupBuilder to create an html xml output
-     */
-    private static doText(HTMLGenerator memoDsl) {
-        String template = "Memo\nTo: ${memoDsl.toText}\nFrom: ${memoDsl.fromText}\n${memoDsl.body}\n"
-        def sectionStrings =""
-        for (s in memoDsl.sections) {
-            sectionStrings += s.title.toUpperCase() + "\n" + s.body + "\n"
+    def static templateTwo(HTMLGenerator htmlDsl) {
+        def markup = new MarkupBuilder(writer)
+
+        markup.html() {
+            head {
+                title("Web Gallery")
+                link(href: "assets/css/bootstrap.min.css", rel: "stylesheet")
+                link(href: "assets/css/bootstrap-theme.min.css", rel:"stylesheet")
+            }
+            body {
+                div class: "container", {
+                    div class: "row", {
+                        div class: "col-md-5", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-3", {
+                            h1("Web Gallery")
+                        }
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                    }
+
+                    div class: "row", {
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-4", {
+                            a href: "#", class: "thumbnail", {
+                                img(src: htmlDsl.profilePicPath, alt:"picture")
+                            }
+                        }
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                    }
+
+                    div class: "row", {
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-4", {
+                            div class: "row", {
+                                div class: "col-md-2", {
+                                    mkp.yield ""
+                                }
+                                div class: "col-md-9", {
+                                    h3(htmlDsl.name)
+                                }
+                                div class: "col-md-1", {
+                                    mkp.yield ""
+                                }
+                            }
+                        }
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                    }
+
+                    div class: "row", {
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-4", {
+                            p(style:"margin-top:2em; font-style: italic; font-size: 12px;", htmlDsl.description)
+                        }
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                    }
+                }
+            }
         }
-        template += sectionStrings
-        println template
+    }
+
+    def static templateThree(HTMLGenerator htmlDsl) {
+        def markup = new MarkupBuilder(writer)
+
+        markup.html() {
+            head {
+                title("Web Gallery")
+                link(href: "assets/css/bootstrap.min.css", rel: "stylesheet")
+                link(href: "assets/css/bootstrap-theme.min.css", rel:"stylesheet")
+            }
+            body {
+                div class: "container", {
+                    div class: "row", {
+                        div class: "col-md-5", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-3", {
+                            h1("Web Gallery")
+                        }
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                    }
+
+                    div class: "row", {
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-4", {
+                            a href: "#", class: "thumbnail", {
+                                img(class: "img-circle", src: htmlDsl.profilePicPath, alt:"picture")
+                            }
+                        }
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                    }
+
+                    div class: "row", {
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-4", {
+                            div class: "row", {
+                                div class: "col-md-2", {
+                                    mkp.yield ""
+                                }
+                                div class: "col-md-9", {
+                                    h3(htmlDsl.name)
+                                }
+                                div class: "col-md-1", {
+                                    mkp.yield ""
+                                }
+                            }
+                        }
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                    }
+
+                    div class: "row", {
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                        div class: "col-md-4", {
+                            p(style:"margin-top:2em; font-style: italic; font-size: 12px;", htmlDsl.description)
+                        }
+                        div class: "col-md-4", {
+                            mkp.yield ""
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static doHtml(HTMLGenerator htmlDsl) {
+        switch (htmlDsl.template) {
+            case 1 :
+                templateOne(htmlDsl);
+                break;
+            case 2 :
+                templateTwo(htmlDsl);
+                break;
+            case 3 :
+                templateThree(htmlDsl);
+                break;
+            default:
+                templateOne(htmlDsl);
+                break;
+        }
+
+        File file = new File("out", "tes.html");
+        if (file.exists()) {
+            file.delete();
+            file.createNewFile();
+        }
+
+        file << writer;
     }
 
     public static void main(String[] args) {
         HTMLGenerator.make {
-            to "Nirav Assar"
-            from "Barack Obama"
-            body "How are things? We are doing well. Take care"
-            idea "The economy is key"
-            request "Please vote for me"
+            name "Lorem Ipsum Dolor Sit"
+            picture "assets//img//landscape0.jpg"
+            description "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\n" +
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\n" +
+                    "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n" +
+                    "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\n" +
+                    "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\n" +
+                    "proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            login false
+            upload false
+            template 1
             html
         }
     }
--- src/main/java/Section.groovy
+++
@@ -1,7 +0,0 @@
-/**
- * Created by rikysamuel on 11/27/2015.
- */
-class Section {
-    String title
-    String body
-}
