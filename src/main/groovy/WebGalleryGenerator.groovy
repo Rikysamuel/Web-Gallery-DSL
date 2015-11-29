79c0485ac0ba1d5df5f20816c19a187ee0b739e6
Rikysamuel <rikz.samuel@gmail.com>
2015-11-29 15:38:55 +0700

    separate fi;e

---
+++ src/main/groovy/WebGalleryGenerator.groovy
@@ -0,0 +1,18 @@
+/**
+ * Created by rikysamuel on 11/29/2015.
+ */
+
+HTMLGenerator.make {
+    name "Lorem Ipsum Dolor Sit"
+    picture "assets//img//landscape0.jpg"
+    description "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\n" +
+            "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\n" +
+            "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n" +
+            "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\n" +
+            "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\n" +
+            "proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    login true
+    upload true
+    template 1
+    html
+}
