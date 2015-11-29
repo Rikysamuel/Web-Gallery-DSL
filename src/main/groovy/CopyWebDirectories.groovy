7db6fbf4d4161e674d2830ff0be8d0a74ae892d4
steve1994 <steve.harnadi@gmail.com>
2015-11-29 16:44:13 +0700

    Copy file content recursive done

--- src/main/groovy/CopyWebDirectories.groovy
+++ src/main/groovy/CopyWebDirectories.groovy
@@ -4,21 +4,24 @@
 class CopyWebDirectories {
 
     /* copy all content folder in spesific path into out folder */
-    public static void moveFileIntoOutFolder(String path) {
+    public static void moveFileIntoOutFolder(String path,String dest) {
         new File(path).eachFile { file->
             if (file.file) {
+                println(file.getAbsolutePath())
                 def src = new File(file.getAbsolutePath())
-                def dst = new File('out\\' + file.getName())
+                def dst = new File(dest + "\\" + file.getName())
                 dst << src.text
             } else {
+                println(file.getAbsolutePath())
+                println('out\\' + file.getName())
                 new File('out\\' + file.getName()).mkdir()
-                moveFileIntoOutFolder(file.getAbsolutePath())
+                moveFileIntoOutFolder(file.getAbsolutePath(),'out\\' + file.getName())
             }
         }
     }
 
     public static void main(String[] arg) {
-        CopyWebDirectories.moveFileIntoOutFolder("D:\\Tes")
+        CopyWebDirectories.moveFileIntoOutFolder("D:\\Tes","out\\")
     }
 }
 
